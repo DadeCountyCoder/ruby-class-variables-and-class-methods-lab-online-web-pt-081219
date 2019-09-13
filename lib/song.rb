465bc168e3350e5d75304579865590cca95764b3
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-13 15:13:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
   #class_variables they belong to the class(ex.mother & kids)(class properties)
-    @@count = 0
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
